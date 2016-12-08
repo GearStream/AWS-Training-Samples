d166a2e8aeb61570b22b9d13480a9ac5289aa557
jamescross91 <james_cross91@hotmail.com>
2016-12-08 17:10:07 +0000

    Updating labs

--- postgres-auth/sql/setup.sql
+++ postgres-auth/sql/setup.sql
@@ -1,8 +1,10 @@
-create database gearstream;
+create database geatstream;
 create schema gearstream;
 
 create table gearstream.user_table(
-user_name text,
-password text,
-first_name text,
-last_name text);
+	user_name text,
+	password text,
+	first_name text,
+	last_name text,
+	token text
+)
