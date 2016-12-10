36c15d8bfd0470cadc2162029ff61c0f283f14fb
jamescross91 <james_cross91@hotmail.com>
2016-12-10 13:40:11 +0000

    Adding setup shell scrip

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+
+sudo yum install -y git
+
+git clone https://github.com/GearStream/AWS-Training-Samples.git
+
+sudo docker build -t gearstream/simple-service AWS-Training-Samples/nginx-docker/
+sudo docker run -d -p 8080:8080 gearstream/simple-service
